fed2f53909ff144a83bb28f6f41c794c23de6a6e
Prashant Prakash Dubey™ <ppdubey98@gmail.com>
2026-05-15 02:30:28 +0530

    vercel 6

--- .vercel/output/static/assets/blog-CvQl3DiK.js
+++
@@ -1,1 +0,0 @@
-import{Y as t,O as o}from"./index-CO9uRJaT.js";const n=()=>t.jsx(o,{});export{n as component};
--- .vercel/output/static/assets/colleges-Cm1XCP5n.js
+++
@@ -1,1 +0,0 @@
-import{Y as e,O as s}from"./index-CO9uRJaT.js";function o(){return e.jsx("div",{className:"min-h-screen bg-background",children:e.jsx(s,{})})}export{o as component};
